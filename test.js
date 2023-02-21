53da93142bea9b5f57f04f21f45c6265d830be37
Milidrag <m-ivkic@gmx.at>
2023-02-21 16:16:13 +0100

    adapted testfile to test the connection to testnet

--- test.js
+++ test.js
@@ -1,27 +1,19 @@
-import { Alchemy } from 'alchemy-sdk';
+const { Client, initLogger } = require('@iota/client');
 
-// Optional Config object, but defaults to demo api-key and eth-mainnet.
-const settings = {
-    apiKey: 'demo', // Replace with your Alchemy API Key.
-    network: Network.ETH_MAINNET, // Replace with your network.
-};
+async function run() {
+    initLogger();
 
-const alchemy = new Alchemy(settings);
+    const client = new Client({
+        nodes: ['https://api.testnet.shimmer.network'],
+        localPow: true,
+    });
 
-// Access standard Ethers.js JSON-RPC node request
-alchemy.core.getBlockNumber().then(console.log);
+    try {
+        const nodeInfo = await client.getInfo();
+        console.log('Node info: ', nodeInfo);
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+}
 
-// Access Alchemy Enhanced API requests
-alchemy.core.getTokenBalances("0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE").then(console.log);
-
-// Access the Alchemy NFT API
-alchemy.nft.getNftsForOwner('vitalik.eth').then(console.log);
-
-// Access WebSockets and Alchemy-specific WS methods
-alchemy.ws.on(
-    {
-        method: 'alchemy_pendingTransactions'
-    },
-    res => console.log(res)
-);
-```
+run().then(() => process.exit());
