df25be680889dadab5954c7814139e05488034ce
Milidrag <m-ivkic@gmx.at>
2023-03-06 15:40:53 +0100

    adapted the hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "shimmerTest",
+  networks: {
+    shimmerTest: {
+      url: "https://api.sc.testnet.shimmer.network/evm/jsonrpc",
+      accounts: ["0xe1ec520d880d5f95b17e97fcfd56ed9a77ecca3c455a9bcaca72ab8543483537"],
+      timeout: 60000
+    }
+  }
 };
