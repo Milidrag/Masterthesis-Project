4658bf8dcd612cfb110aedb9a51a84b456036d60
Milidrag <m-ivkic@gmx.at>
2023-03-10 11:38:38 +0100

    tx worked. also the hash from ipfs is successfully stored. measurement works

--- transfer.js
+++ transfer.js
@@ -18,97 +18,97 @@ const binary = fs.readFileSync("./1_Storage_sol_Storage.bin", "utf8");
 
 
 async function main(contract) {
-    /*  jsonString = fs.readFileSync("./data.json", "utf-8");
-     jsonStringFirst = JSON.parse(jsonString)[0];
- 
-     console.log("Start creating connection to IPFS client...")
-     //IPFS start
-     const { create } = await import('ipfs-http-client')
-     const client = await create(
-         {
-             host: "ipfs.infura.io",
-             port: 5001,
-             protocol: "https",
-             headers: {
-                 "Authorization": `Basic ${Buffer.from(INFURA_PROJECT_ID + ':' + INFURA_API_SECRET).toString("base64")}`
-             }
-         })
- 
-     console.log("IPFS client Connection established")
- 
-     console.log("Start calling getHash-Function...")
-     //take the hash from the contract and call with the client the data
-     const startGetHash = Date.now();
-     const hashOldData = await contract.getHash();
-     const endGetHash = Date.now()
-     const durationGetHash = endGetHash - startGetHash;
-     console.log("Writing duration of calling getHash function inside duration-GET-HASH.txt...")
-     fs.appendFile("./duration-GET-HASH.txt", durationGetHash.toString() + "\n", err => {     //writing duration to file
-         if (err) {
-             s
-             console.log(err)
-         }
-     })
- 
-     // create a string to append contents to
-     let contents = ""
-     console.log("here")
-     console.log(hashOldData)
-     if (hashOldData.length > 2) {
-         console.log("Get IPFS data with the hash stored on the SC...")
-         const startGetHashFromIpfs = Date.now();
-         const ipfsObject = await client.get(hashOldData)
-         const endGetHashFromIpfs = Date.now();
-         const durationGetHashFromIpfs = endGetHashFromIpfs - startGetHashFromIpfs;
-         console.log("Writing duration of getting hash from ipfs inside duration-GET-HASH-FROM-IPFS.txt...")
-         fs.appendFile("./duration-GET-HASH-FROM-IPFS.txt", durationGetHashFromIpfs.toString() + "\n", err => {     //writing duration to file
-             if (err) {
-                 console.log(err)
-             }
-         })
- 
-         // loop over incoming data
-         for await (const item of ipfsObject) {
-             // turn string buffer to string and append to contents
-             contents += new TextDecoder().decode(item)
-         }
- 
-         // remove null characters
-         contents = contents.replace(/\0/g, "")
-     }
- 
-     console.log("Constructing new data for IPFS...")
- 
- 
-     //concat old values with jsonStringFirst
-     let jsonArray = []
-     console.log("concat..")
-     if (contents.length > 2) { //in case a new contract is deployed and the hash value is empty
-         const ipfsData = JSON.parse(contents.substring(contents.indexOf('['))) //find array and construct a substring
-         ipfsData.push(jsonStringFirst) //push new value to exisiting array
-         jsonArray = ipfsData //assign to 
-     } else {
-         jsonArray.push(jsonStringFirst)
-     }
- 
-     console.log("Sending data to IPFS...")
-     //send data
-     await client.add(JSON.stringify(jsonArray)).then((res) => {
-         //here I have to call the contract and send the hash
-         console.log("Sending new hash to contract...")
-         console.log(res)
-         const startSetHash = Date.now();
-         transactionResponseSetHash = contract.sendHash(res.path)
-         const endSetHash = Date.now()
-         const durationSetHash = endSetHash - startSetHash
-         console.log("Writing duration of calling setHash function inside duration-SET-HASH.txt...")
-         fs.appendFile("./duration-SET-HASH.txt", durationSetHash.toString() + "\n", err => {     //writing duration to file
-             if (err) {
-                 console.log(err)
-             }
-         })
-     });
-  */
+    jsonString = fs.readFileSync("./data.json", "utf-8");
+    jsonStringFirst = JSON.parse(jsonString)[0];
+
+    console.log("Start creating connection to IPFS client...")
+    //IPFS start
+    const { create } = await import('ipfs-http-client')
+    const client = await create(
+        {
+            host: "ipfs.infura.io",
+            port: 5001,
+            protocol: "https",
+            headers: {
+                "Authorization": `Basic ${Buffer.from(INFURA_PROJECT_ID + ':' + INFURA_API_SECRET).toString("base64")}`
+            }
+        })
+
+    console.log("IPFS client Connection established")
+
+    console.log("Start calling getHash-Function...")
+    //take the hash from the contract and call with the client the data
+    const startGetHash = Date.now();
+    const hashOldData = await contract.getHash();
+    const endGetHash = Date.now()
+    const durationGetHash = endGetHash - startGetHash;
+    console.log("Writing duration of calling getHash function inside duration-GET-HASH.txt...")
+    fs.appendFile("./duration-GET-HASH.txt", durationGetHash.toString() + "\n", err => {     //writing duration to file
+        if (err) {
+            s
+            console.log(err)
+        }
+    })
+
+    // create a string to append contents to
+    let contents = ""
+    console.log("here")
+    console.log(hashOldData)
+    if (hashOldData.length > 2) {
+        console.log("Get IPFS data with the hash stored on the SC...")
+        const startGetHashFromIpfs = Date.now();
+        const ipfsObject = await client.get(hashOldData)
+        const endGetHashFromIpfs = Date.now();
+        const durationGetHashFromIpfs = endGetHashFromIpfs - startGetHashFromIpfs;
+        console.log("Writing duration of getting hash from ipfs inside duration-GET-HASH-FROM-IPFS.txt...")
+        fs.appendFile("./duration-GET-HASH-FROM-IPFS.txt", durationGetHashFromIpfs.toString() + "\n", err => {     //writing duration to file
+            if (err) {
+                console.log(err)
+            }
+        })
+
+        // loop over incoming data
+        for await (const item of ipfsObject) {
+            // turn string buffer to string and append to contents
+            contents += new TextDecoder().decode(item)
+        }
+
+        // remove null characters
+        contents = contents.replace(/\0/g, "")
+    }
+
+    console.log("Constructing new data for IPFS...")
+
+
+    //concat old values with jsonStringFirst
+    let jsonArray = []
+    console.log("concat..")
+    if (contents.length > 2) { //in case a new contract is deployed and the hash value is empty
+        const ipfsData = JSON.parse(contents.substring(contents.indexOf('['))) //find array and construct a substring
+        ipfsData.push(jsonStringFirst) //push new value to exisiting array
+        jsonArray = ipfsData //assign to 
+    } else {
+        jsonArray.push(jsonStringFirst)
+    }
+
+    console.log("Sending data to IPFS...")
+    //send data
+    await client.add(JSON.stringify(jsonArray)).then((res) => {
+        //here I have to call the contract and send the hash
+        console.log("Sending new hash to contract...")
+        console.log(res)
+        const startSetHash = Date.now();
+        transactionResponseSetHash = contract.sendHash(res.path)
+        const endSetHash = Date.now()
+        const durationSetHash = endSetHash - startSetHash
+        console.log("Writing duration of calling setHash function inside duration-SET-HASH.txt...")
+        fs.appendFile("./duration-SET-HASH.txt", durationSetHash.toString() + "\n", err => {     //writing duration to file
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+
     //IPFS end
     const transaction = {
         to: CONTRACT_ADDRESS_MUMBAI,
