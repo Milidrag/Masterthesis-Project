8ba16e1af47c9ec9438df4d37eed4076d9db43e1
Milidrag <m-ivkic@gmx.at>
2023-03-20 21:28:09 +0100

    added gasReporter test measurement for transfer function

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,6 +18,8 @@ module.exports = {
   gasReporter: {
     enabled: true,
     currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
     coinmarketcap: COINMARKET
   }
 };
--- test/Storage-test.js
+++ test/Storage-test.js
@@ -3,31 +3,45 @@ const hre = require("hardhat");
 
 describe("Gas cost measurement for store function", function () {
 
-  let contractFactory, storage
-  beforeEach(async function () {
-    contractFactory = await hre.ethers.getContractFactory("Storage");
-    storage = await contractFactory.attach(                                     //here the contract object is attached to contract address 
-      process.env.CONTRACT_ADDRESS_GANACHE
-    );
-  })
+    let contractFactory, storage
+    beforeEach(async function () {
+        contractFactory = await ethers.getContractFactory("Storage")
+        storage = await contractFactory.deploy()
+    })
 
-  it("Should measure gas cost of store function", async function () {
+    it("Should measure gas cost of store function", async function () {
 
-    const iotData = JSON.stringify({
-      "Axis-X": 1067,
-      "Axis-Y": 1670,
-      "Axis-Z": 1179
+        const iotData = JSON.stringify({
+            "Axis-X": 1067,
+            "Axis-Y": 1670,
+            "Axis-Z": 1179
+        });
+
+        // Measure gas cost of store function
+        const startTime = Date.now();
+        const tx = await storage.store(iotData);
+        const receipt = await tx.wait();
+        const duration = Date.now() - startTime;
+
+
+        console.log("Gas used for store function:", receipt.gasUsed.toString());
+        console.log("Duration of store function:", duration.toString(), "ms");
+
     });
 
-    // Measure gas cost of store function
-    const startTime = Date.now();
-    const tx = await storage.store(iotData);
-    const receipt = await tx.wait();
-    const duration = Date.now() - startTime;
+    it("Should measure gas cost of transfer function", async function () {
+
+        // Measure gas cost of store function
+        const startTime = Date.now();
+        const tx = await storage.transfer({
+            value: hre.ethers.utils.parseUnits("0.001", "ether"),
+        });
+        const receipt = await tx.wait();
+        const duration = Date.now() - startTime;
 
 
-    console.log("Gas used for store function:", receipt.gasUsed.toString());
-    console.log("Duration of store function:", duration.toString(), "ms");
+        console.log("Gas used for transfer function:", receipt.gasUsed.toString());
+        console.log("Duration of transfer function:", duration.toString(), "ms");
 
-  });
+    });
 });
