8c0267545b0768a16f322d7bca35b25ddf1bac5a
Milidrag <m-ivkic@gmx.at>
2023-03-20 10:32:32 +0100

    refactored deploy.js to use hardhat

--- deploy.js
+++ deploy.js
@@ -1,28 +1,14 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require('dotenv').config();
+const hre = require("hardhat");
 
 
 async function main() {
-    //compile them in our code 
-    //compile them separately 
-    //http://0.0.0.0:7545
-    const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_ALCHEMY);
-    const wallet = new ethers.Wallet(
-        process.env.PRIVATE_KEY_GOERLI_ALICE,
-        provider
-    );
-    const abi = fs.readFileSync("./1_Storage_sol_Storage.abi", "utf8");
-    const binary = fs.readFileSync("./1_Storage_sol_Storage.bin", "utf8");
 
-    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-    console.log("Deploying, please wait...");
-    const contract = await contractFactory.deploy(); // STOP here! Wait for contract to deploy
-    console.log("This is the address of your contract");
-    console.log(contract.address);
-    const transactionReceipt = await contract.deployTransaction.wait(1);
-    console.log("Here is the deployment transaction (transaction response): ");
-    /* console.log(transactionReceipt); */
+    const Storage = await hre.ethers.getContractFactory("Storage");
+    const storage = await Storage.deploy();
+
+    await storage.deployed();
+
+    console.log("Here is the contract address: " + storage.address);
 
 }
 
