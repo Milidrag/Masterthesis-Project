259dcdb84e29da0058fbedcc225cd0ab9296d682
Milidrag <m-ivkic@gmx.at>
2023-03-20 10:39:53 +0100

    adapted the hardhat-config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { PROVIDER_GANACHE, PROVIDER_ALCHEMY, COINMARKET } = process.env
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.7",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: PROVIDER_ALCHEMY,
+      accounts: [PRIVATE_KEY_GANACHE_BOB],
+      chainID: 5
+    }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: COINMARKET
+  }
 };
