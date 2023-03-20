7e2438276c120c1cebe0d682359481767dcc61cd
Milidrag <m-ivkic@gmx.at>
2023-03-20 21:34:56 +0100

    added gasReport measurement test for transfer-function

--- test/Storage-test.js
+++ test/Storage-test.js
@@ -22,4 +22,20 @@ describe("Gas cost measurement for store function", function () {
         console.log("Gas used for store function:", receipt.gasUsed.toString());
         console.log("Duration of store function:", duration.toString(), "ms");
     });
+
+    it("Should measure gas cost of transfer function", async function () {
+
+        // Measure gas cost of store function
+        const startTime = Date.now();
+        const tx = await storage.transfer({
+            value: hre.ethers.utils.parseUnits("0.001", "ether"),
+        });
+        const receipt = await tx.wait();
+        const duration = Date.now() - startTime;
+
+
+        console.log("Gas used for transfer function:", receipt.gasUsed.toString());
+        console.log("Duration of transfer function:", duration.toString(), "ms");
+
+    });
 })
