e434d54c2c1816a44b6aee97081442c3d96050b5
Milidrag <m-ivkic@gmx.at>
2023-03-21 13:38:48 +0100

    added test for ipfs

--- test/Storage-test.js
+++ test/Storage-test.js
@@ -1,11 +1,10 @@
 const { ethers } = require("hardhat")
-
-
 const { INFURA_PROJECT_ID, INFURA_API_SECRET } = process.env;
+const fs = require("fs");
 
 
 describe("Gas cost measurement", function () {
-    let storageFactory, storage, hash, client
+    let storageFactory, storage, hashOldData, client, ipfsData
 
     it("Should create a client connection", async function () {
         const { create } = await import('ipfs-http-client')
@@ -28,11 +27,11 @@ describe("Gas cost measurement", function () {
 
     it("Should store content on IPFS and take hash", async function () {
 
-        const iotData = JSON.stringify({
+        const iotData = JSON.stringify([{
             "Axis-X": 1067,
             "Axis-Y": 1670,
             "Axis-Z": 1179
-        });
+        }]);
 
         await client.add(iotData).then(async (res) => {
             console.log("Sending new hash to contract...")
@@ -43,45 +42,57 @@ describe("Gas cost measurement", function () {
             const receipt = await tx.wait();
             const duration = endSetHash - startSetHash
 
-            console.log("Gas used for store function:", receipt.gasUsed.toString());
-            console.log("Duration of store function:", duration.toString(), "ms");
+            console.log("Gas used for sendHash function:", receipt.gasUsed.toString());
+            console.log("Duration of sendHash function:", duration.toString(), "ms");
         });
+    })
 
+    it("Should get hash from contract", async function () {
+        const startGetHash = Date.now();
+        hashOldData = await storage.getHash();
+        const endGetHash = Date.now()
+        const duration = endGetHash - startGetHash;
 
+        console.log("Duration of getHash function:", duration.toString(), "ms");
+    })
+
+    it("Should get data from IPFS", async function () {
+        const ipfsObject = await client.get(hashOldData)
+        // loop over incoming data
+        let contents = ""
+        for await (const item of ipfsObject) {
+            // turn string buffer to string and append to contents
+            contents += new TextDecoder().decode(item)
+        }
+
+        // remove null characters
+        contents = contents.replace(/\0/g, "")
+        ipfsData = JSON.parse(contents.substring(contents.indexOf('['))) //find array and construct a substring
 
     })
 
+    it("Should concat data", async function () {
+        const iotDataNew = {
+            "Axis-X": 1578,
+            "Axis-Y": 1227,
+            "Axis-Z": 431
+        };
 
-    /*     it("Gas cost measurement for store function", async function () {
-            const iotData = JSON.stringify({
-                "Axis-X": 1067,
-                "Axis-Y": 1670,
-                "Axis-Z": 1179
-            });
-    
-            // Measure gas cost of store function
-            const startTime = Date.now();
-            const tx = await storage.store(iotData);
+        ipfsData.push(iotDataNew)
+    })
+
+    it("Should send the concat data to IPFS", async function () {
+        await client.add(JSON.stringify(ipfsData)).then(async (res) => {
+            console.log("Sending new hash to contract...")
+            console.log(res)
+            const startSetHash = Date.now();
+            const tx = await storage.sendHash(res.path)
+            const endSetHash = Date.now()
             const receipt = await tx.wait();
-            const duration = Date.now() - startTime;
-    
-            console.log("Gas used for store function:", receipt.gasUsed.toString());
-            console.log("Duration of store function:", duration.toString(), "ms");
+            const duration = endSetHash - startSetHash
+
+            console.log("Gas used for sendHash function with concat data:", receipt.gasUsed.toString());
+            console.log("Duration of sendHash function with concat data:", duration.toString(), "ms");
         });
-    
-        it("Should measure gas cost of transfer function", async function () {
-    
-            // Measure gas cost of store function
-            const startTime = Date.now();
-            const tx = await storage.transfer({
-                value: hre.ethers.utils.parseUnits("0.001", "ether"),
-            });
-            const receipt = await tx.wait();
-            const duration = Date.now() - startTime;
-    
-    
-            console.log("Gas used for transfer function:", receipt.gasUsed.toString());
-            console.log("Duration of transfer function:", duration.toString(), "ms");
-    
-        }); */
+    })
 })
