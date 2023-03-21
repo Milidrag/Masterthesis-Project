902bc7988f5e17a6074f9c377319a0973bb562b4
Milidrag <m-ivkic@gmx.at>
2023-03-21 14:58:50 +0100

    refactored deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("hardhat-gas-reporter")
+
+
+const { PRIVATE_KEY_GOERLI_ALICE, PROVIDER_ALCHEMY_POLYGON, COINMARKET, ETHEREUM_EXPLORER_API_KEY } = process.env
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.7",
+  defaultNetwork: "mumbai",
+  networks: {
+    mumbai: {
+      url: PROVIDER_ALCHEMY_POLYGON,
+      accounts: [PRIVATE_KEY_GOERLI_ALICE],
+      chainID: 80001
+    }
+  },
+  etherscan: {
+    apiKey: ETHEREUM_EXPLORER_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKET,
+    token: "MATIC"
+  }
 };
