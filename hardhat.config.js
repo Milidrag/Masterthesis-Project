7c15dac0867625324180b9f0983fb6e4a8978113
Milidrag <m-ivkic@gmx.at>
2023-03-21 10:00:47 +0100

    added .env for convience

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("hardhat-gas-reporter")
+
+
+const { PRIVATE_KEY_GOERLI_ALICE, PROVIDER_ALCHEMY, COINMARKET, ETHEREUM_EXPLORER_API_KEY, PROVIDER_ALCHEMY_SEPOLIA } = process.env
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.7",
+  defaultNetwork: "sepolia",
+  networks: {
+    goerli: {
+      url: PROVIDER_ALCHEMY,
+      accounts: [PRIVATE_KEY_GOERLI_ALICE],
+      chainID: 5
+    },
+    sepolia: {
+      url: PROVIDER_ALCHEMY_SEPOLIA,
+      accounts: [PRIVATE_KEY_GOERLI_ALICE],
+      chainId: 11155111,
+    }
+  },
+  etherscan: {
+    apiKey: ETHEREUM_EXPLORER_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKET
+  }
 };
