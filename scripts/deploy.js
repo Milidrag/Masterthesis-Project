deddc101e345033eb54e047f65dc1e110b4527de
Milidrag <m-ivkic@gmx.at>
2023-03-21 09:57:35 +0100

    adapted deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+
+async function main() {
+
+    const Storage = await hre.ethers.getContractFactory("Storage");
+    const storage = await Storage.deploy();
+
+    await storage.deployed();
+
+    console.log("Here is the contract address: " + storage.address);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
